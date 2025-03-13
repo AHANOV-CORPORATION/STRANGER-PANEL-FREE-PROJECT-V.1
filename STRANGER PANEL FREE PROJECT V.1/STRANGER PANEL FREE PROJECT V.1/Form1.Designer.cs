e3b3149524a11729c2dc9b840742116db4b4a49c
𝐒𝐓𝐑𝐀𝐍𝐆𝐄𝐑 𝐏𝐀𝐍𝐄𝐋 𝐂𝐎𝐑𝐏𝐎𝐑𝐀𝐓𝐈𝐎𝐍 <tastingpanel71@gmail.com>
2025-03-13 12:26:37 -0700

    Add files via upload

---
+++ STRANGER PANEL FREE PROJECT V.1/STRANGER PANEL FREE PROJECT V.1/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace STRANGER_PANEL_FREE_PROJECT_V._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.SPC_USER = new Guna.UI2.WinForms.Guna2TextBox();
+            this.SPC_PASS = new Guna.UI2.WinForms.Guna2TextBox();
+            this.SPC_KEY = new Guna.UI2.WinForms.Guna2TextBox();
+            this.SPC_LOGIN = new Guna.UI2.WinForms.Guna2Button();
+            this.SPC_REG = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Separator1 = new Guna.UI2.WinForms.Guna2Separator();
+            this.guna2Separator2 = new Guna.UI2.WinForms.Guna2Separator();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.SPC_STA = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.SuspendLayout();
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.guna2HtmlLabel1.IsSelectionEnabled = false;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(122, 7);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(294, 39);
+            this.guna2HtmlLabel1.TabIndex = 0;
+            this.guna2HtmlLabel1.Text = "STRANGER PANEL";
+            // 
+            // SPC_USER
+            // 
+            this.SPC_USER.Animated = true;
+            this.SPC_USER.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.SPC_USER.BorderRadius = 9;
+            this.SPC_USER.BorderThickness = 2;
+            this.SPC_USER.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.SPC_USER.DefaultText = "";
+            this.SPC_USER.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.SPC_USER.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.SPC_USER.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.SPC_USER.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.SPC_USER.FillColor = System.Drawing.Color.Transparent;
+            this.SPC_USER.FocusedState.BorderColor = System.Drawing.Color.DarkViolet;
+            this.SPC_USER.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SPC_USER.ForeColor = System.Drawing.Color.Silver;
+            this.SPC_USER.HoverState.BorderColor = System.Drawing.Color.Silver;
+            this.SPC_USER.Location = new System.Drawing.Point(49, 71);
+            this.SPC_USER.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.SPC_USER.Name = "SPC_USER";
+            this.SPC_USER.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.SPC_USER.PlaceholderText = "SPC-USERNAME";
+            this.SPC_USER.SelectedText = "";
+            this.SPC_USER.Size = new System.Drawing.Size(440, 43);
+            this.SPC_USER.TabIndex = 1;
+            // 
+            // SPC_PASS
+            // 
+            this.SPC_PASS.Animated = true;
+            this.SPC_PASS.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.SPC_PASS.BorderRadius = 9;
+            this.SPC_PASS.BorderThickness = 2;
+            this.SPC_PASS.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.SPC_PASS.DefaultText = "";
+            this.SPC_PASS.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.SPC_PASS.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.SPC_PASS.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.SPC_PASS.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.SPC_PASS.FillColor = System.Drawing.Color.Transparent;
+            this.SPC_PASS.FocusedState.BorderColor = System.Drawing.Color.DarkViolet;
+            this.SPC_PASS.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SPC_PASS.ForeColor = System.Drawing.Color.Silver;
+            this.SPC_PASS.HoverState.BorderColor = System.Drawing.Color.Silver;
+            this.SPC_PASS.Location = new System.Drawing.Point(49, 134);
+            this.SPC_PASS.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.SPC_PASS.Name = "SPC_PASS";
+            this.SPC_PASS.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.SPC_PASS.PlaceholderText = "SPC-PASSWORD";
+            this.SPC_PASS.SelectedText = "";
+            this.SPC_PASS.Size = new System.Drawing.Size(440, 43);
+            this.SPC_PASS.TabIndex = 1;
+            // 
+            // SPC_KEY
+            // 
+            this.SPC_KEY.Animated = true;
+            this.SPC_KEY.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.SPC_KEY.BorderRadius = 9;
+            this.SPC_KEY.BorderThickness = 2;
+            this.SPC_KEY.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.SPC_KEY.DefaultText = "";
+            this.SPC_KEY.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.SPC_KEY.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.SPC_KEY.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.SPC_KEY.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.SPC_KEY.FillColor = System.Drawing.Color.Transparent;
+            this.SPC_KEY.FocusedState.BorderColor = System.Drawing.Color.DarkViolet;
+            this.SPC_KEY.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SPC_KEY.ForeColor = System.Drawing.Color.Silver;
+            this.SPC_KEY.HoverState.BorderColor = System.Drawing.Color.Silver;
+            this.SPC_KEY.Location = new System.Drawing.Point(49, 195);
+            this.SPC_KEY.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.SPC_KEY.Name = "SPC_KEY";
+            this.SPC_KEY.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.SPC_KEY.PlaceholderText = "SPC-KEY";
+            this.SPC_KEY.SelectedText = "";
+            this.SPC_KEY.Size = new System.Drawing.Size(440, 43);
+            this.SPC_KEY.TabIndex = 1;
+            // 
+            // SPC_LOGIN
+            // 
+            this.SPC_LOGIN.Animated = true;
+            this.SPC_LOGIN.BorderColor = System.Drawing.Color.DimGray;
+            this.SPC_LOGIN.BorderRadius = 9;
+            this.SPC_LOGIN.BorderThickness = 2;
+            this.SPC_LOGIN.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.SPC_LOGIN.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.SPC_LOGIN.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.SPC_LOGIN.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.SPC_LOGIN.FillColor = System.Drawing.Color.Transparent;
+            this.SPC_LOGIN.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SPC_LOGIN.ForeColor = System.Drawing.Color.White;
+            this.SPC_LOGIN.HoverState.BorderColor = System.Drawing.Color.DarkViolet;
+            this.SPC_LOGIN.HoverState.ForeColor = System.Drawing.Color.BlueViolet;
+            this.SPC_LOGIN.Location = new System.Drawing.Point(117, 257);
+            this.SPC_LOGIN.Name = "SPC_LOGIN";
+            this.SPC_LOGIN.Size = new System.Drawing.Size(296, 45);
+            this.SPC_LOGIN.TabIndex = 2;
+            this.SPC_LOGIN.Text = "LOGIN";
+            this.SPC_LOGIN.Click += new System.EventHandler(this.SPC_LOGIN_Click);
+            // 
+            // SPC_REG
+            // 
+            this.SPC_REG.Animated = true;
+            this.SPC_REG.BorderColor = System.Drawing.Color.DimGray;
+            this.SPC_REG.BorderRadius = 9;
+            this.SPC_REG.BorderThickness = 2;
+            this.SPC_REG.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.SPC_REG.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.SPC_REG.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.SPC_REG.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.SPC_REG.FillColor = System.Drawing.Color.Transparent;
+            this.SPC_REG.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SPC_REG.ForeColor = System.Drawing.Color.White;
+            this.SPC_REG.HoverState.BorderColor = System.Drawing.Color.DarkViolet;
+            this.SPC_REG.HoverState.ForeColor = System.Drawing.Color.BlueViolet;
+            this.SPC_REG.Location = new System.Drawing.Point(117, 318);
+            this.SPC_REG.Name = "SPC_REG";
+            this.SPC_REG.Size = new System.Drawing.Size(296, 45);
+            this.SPC_REG.TabIndex = 2;
+            this.SPC_REG.Text = "REGISTER";
+            // 
+            // guna2Separator1
+            // 
+            this.guna2Separator1.FillColor = System.Drawing.Color.Gray;
+            this.guna2Separator1.FillThickness = 2;
+            this.guna2Separator1.Location = new System.Drawing.Point(-1, 50);
+            this.guna2Separator1.Name = "guna2Separator1";
+            this.guna2Separator1.Size = new System.Drawing.Size(545, 10);
+            this.guna2Separator1.TabIndex = 3;
+            // 
+            // guna2Separator2
+            // 
+            this.guna2Separator2.FillColor = System.Drawing.Color.Gray;
+            this.guna2Separator2.FillThickness = 2;
+            this.guna2Separator2.Location = new System.Drawing.Point(-1, 379);
+            this.guna2Separator2.Name = "guna2Separator2";
+            this.guna2Separator2.Size = new System.Drawing.Size(545, 10);
+            this.guna2Separator2.TabIndex = 3;
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel2.ForeColor = System.Drawing.Color.Silver;
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(-1, 395);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(50, 18);
+            this.guna2HtmlLabel2.TabIndex = 4;
+            this.guna2HtmlLabel2.Text = "Status : ";
+            // 
+            // SPC_STA
+            // 
+            this.SPC_STA.BackColor = System.Drawing.Color.Transparent;
+            this.SPC_STA.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SPC_STA.ForeColor = System.Drawing.Color.Silver;
+            this.SPC_STA.Location = new System.Drawing.Point(55, 395);
+            this.SPC_STA.Name = "SPC_STA";
+            this.SPC_STA.Size = new System.Drawing.Size(3, 2);
+            this.SPC_STA.TabIndex = 4;
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(518, 0);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(28, 27);
+            this.guna2ControlBox1.TabIndex = 5;
+            this.guna2ControlBox1.Click += new System.EventHandler(this.guna2ControlBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(11)))));
+            this.ClientSize = new System.Drawing.Size(545, 422);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.Controls.Add(this.SPC_STA);
+            this.Controls.Add(this.guna2HtmlLabel2);
+            this.Controls.Add(this.guna2Separator2);
+            this.Controls.Add(this.guna2Separator1);
+            this.Controls.Add(this.SPC_REG);
+            this.Controls.Add(this.SPC_LOGIN);
+            this.Controls.Add(this.SPC_KEY);
+            this.Controls.Add(this.SPC_PASS);
+            this.Controls.Add(this.SPC_USER);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2TextBox SPC_USER;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2TextBox SPC_KEY;
+        private Guna.UI2.WinForms.Guna2TextBox SPC_PASS;
+        private Guna.UI2.WinForms.Guna2Button SPC_LOGIN;
+        private Guna.UI2.WinForms.Guna2HtmlLabel SPC_STA;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2Separator guna2Separator2;
+        private Guna.UI2.WinForms.Guna2Separator guna2Separator1;
+        private Guna.UI2.WinForms.Guna2Button SPC_REG;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+    }
+}
+
---
+++ STRANGER PANEL FREE PROJECT V.1/STRANGER PANEL FREE PROJECT V.1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KeyAuth;
+
+namespace STRANGER_PANEL_FREE_PROJECT_V._1
+{
+    public partial class Form1: Form
+    {
+
+
+        public static api KeyAuthApp = new api(
+    name: "SPC-FREE", // App name
+    ownerid: "XOiF3trjE1", // Account ID
+    version: "1.0" // Application version. Used for automatic downloads see video here https://www.youtube.com/watch?v=kW195PLCBKs
+                   //path: @"Your_Path_Here" // (OPTIONAL) see tutorial here https://www.youtube.com/watch?v=I9rxt821gMk&t=1s
+);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2ControlBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private async void SPC_LOGIN_Click(object sender, EventArgs e)
+        {
+            await KeyAuthApp.login(SPC_USER.Text, SPC_PASS.Text, SPC_KEY.Text);
+            if (KeyAuthApp.response.success)
+            {
+                Form2 spc = new Form2();
+                spc.Show();
+                this.Hide();
+            }
+            else
+                //MessageBox.Show("Status: " + KeyAuthApp.response.message);
+                SPC_STA.Text = ("" + KeyAuthApp.response.message);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KeyAuthApp.init();
+        }
+    }
+}
